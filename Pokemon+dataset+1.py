dab2dc535949b4c28a4bd6c6d5d3d01620cb82b0
George <georgeblu97@yahoo.com>
2018-03-17 01:15:25 +0800

    Update Pokemon+dataset+1.py
    Added in new scatter, hexplot and bar stacked and line too

--- Pokemon+dataset+1.py
+++ Pokemon+dataset+1.py
@@ -29,3 +29,11 @@ pokemon['hp'].value_counts().sort_index().plot.line()
 
 pokemon['weight_kg'].plot.hist()
 
+pokemon.plot.scatter(x = 'Attack', y = 'Defense')
+pokemon.plot.hexbin(x = 'Attack', y = 'Defense', gridsize = 15)
+pokemon_stats_legendary = pokemon.groupby(['Legendary', 'Generation']).mean()[['Attack', 'Defense']]
+
+pokemon_stats_legendary.plot.bar(stacked=True)
+
+pokemon_stats_by_generation = pokemon.groupby('Generation').mean()[['HP', 'Attack', 'Defense', 'Sp. Atk', 'Sp. Def', 'Speed']]
+pokemon_stats_by_generation.plot.line()
